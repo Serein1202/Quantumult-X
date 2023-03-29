8fe54cc93b0b9b94f11795174e0a237b9fafc9ac
Crystal0504 <www.1426458287@outlook.com>
2023-03-29 17:52:19 +0800

    2023-3-29 17:27

--- BoxJs/bilibili_live.cookie.js
+++ BoxJs/bilibili_live.cookie.js
@@ -1,3 +1,4 @@
+#添加前请检查MITM中是否存在*.bilibili.com主机名
 # 189及以前版本
 # ^https:\/\/(www|live)\.bilibili\.com\/?.? url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/bilibili/bilibili.cookie.js
 # 190及以后版本
