b9a6a1df7158967ea73b5d19b74ec7db7154932d
Crystal0504 <www.1426458287@outlook.com>
2023-03-29 17:28:00 +0800

    2023-3-29 17:27

--- BoxJs/bilibili_live.cookie.js
+++ BoxJs/bilibili_live.cookie.js
@@ -1,4 +1,4 @@
 # 189及以前版本
-#^https:\/\/(www|live)\.bilibili\.com\/?.? url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/bilibili/bilibili.cookie.js
+# ^https:\/\/(www|live)\.bilibili\.com\/?.? url script-response-body https://raw.githubusercontent.com/chavyleung/scripts/master/bilibili/bilibili.cookie.js
 # 190及以后版本
 ^https:\/\/(www|live)\.bilibili\.com\/?.? url script-request-header https://raw.githubusercontent.com/chavyleung/scripts/master/bilibili/bilibili.cookie.js
